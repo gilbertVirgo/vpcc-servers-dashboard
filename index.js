9e0e10905dec9d659c86950aa850de79df63d23b
Gil <gilbertjvirgo@gmail.com>
2024-05-16 15:51:54 +0100

    Added check/creation of private dir on server launch (and 'public' route)

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+import config from "./config.js";
 import cron from "node-cron";
 import customParseFormat from "dayjs/plugin/customParseFormat.js";
 import dayjs from "dayjs";
 import dotenv from "dotenv";
 import express from "express";
-import fs from "fs/promises";
+import fs from "fs";
 import getCachedData from "./helpers/getCachedData.js";
 import getDirname from "./helpers/getDirname.js";
 import getNextSunday from "./helpers/getNextSunday.js";
@@ -22,16 +23,12 @@ const server = express();
 cron.schedule("* * * * *", () => refreshCache());
 
 const init = async () => {
+	if (!fs.existsSync(config.privateDirectoryPath))
+		fs.mkdirSync(config.privateDirectoryPath);
+
 	await refreshCache();
 
-	const mainCSSFile = await fs.readFile(
-		path.join(getDirname(import.meta.url), "main.css")
-	);
-
-	server.get("/css", (req, res) => {
-		res.setHeader("Content-Type", "text/css");
-		res.send(mainCSSFile);
-	});
+	server.use(express.static("public"));
 
 	server.get("/", (req, res) => {
 		res.redirect("/" + getThisSunday());
@@ -54,11 +51,6 @@ const init = async () => {
 
 		if (date.isBefore(dayjs(), "day"))
 			return res.redirect("/" + getThisSunday());
-
-		// Optimization suggestion: fetch the serverData and store
-		// in a local file (perhaps every 60 seconds). Then read
-		// the local file on GET /:date for considerably quicker
-		// load times on the client.
 
 		const serverData = await getCachedData();
 
